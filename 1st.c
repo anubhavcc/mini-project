2ad90dc6f9d8e42b5c75e32e84a37ccafbdc3f01
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-06 10:07:17 +0530

    Sum function added

--- 1st.c
+++ 1st.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 #include"a.h"
+int sum();
 int main()
 {
 	printf("calling foo function\n"); 
 	foo();
+	int a,b;
+	a=10;b=20; 
 	printf(" foo function called\n");
+	printf("the sum is:%d",sum(a,b));
+
 	return 0;
 }
-
+int sum(int a,int b)
+{
+	return a+b;
+}
 
 
 
