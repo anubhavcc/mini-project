d0fa556ef8d50dca2ed5432e2e25186c60d3e603
Anubhav Choudhary <456anubhav@gmail.com>
2017-06-09 21:59:21 +0530

    cmake files

---
+++ mini-proj/CMakeFiles/pro.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pro.dir/Main.c.o"
+  "CMakeFiles/pro.dir/foo.c.o"
+  "pro.pdb"
+  "pro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
