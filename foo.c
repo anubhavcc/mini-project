3e84b8b71f504f11d415ed41754e06ce956261cd
Nikhil Acharya Prakash <niks3089@gmail.com>
2017-06-09 17:15:28 +0530

    Create foo.c
    Remove unwanted lines. Leave space between mathematical operations and after ","

--- foo.c
+++ foo.c
@@ -1,10 +1,8 @@
+#include <stdio.h>
+#include "foo.h"
 
-
-#include<stdio.h>
-#include"foo.h"
 int foo(int number_1, int number_2) {
-
-	printf("Sum is :%d", number_1+number_2);
+	printf("Sum is :%d", number_1 + number_2);
 	return 0;
 }
 
